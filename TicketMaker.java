ab35cc1e7e26a2b7d7feac6ffc9c84c8f35d77f0
yuto0827 <56053712+yuto0827@users.noreply.github.com>
2020-01-28 22:08:24 +0900

    Add files via upload

---
+++ TicketMaker.java
@@ -0,0 +1,17 @@
+package kadai02;
+
+public class TicketMaker {
+	private int ticket = 1000;
+	private static TicketMaker singleton = new TicketMaker();
+	private TicketMaker() {
+
+	}
+
+	public static TicketMaker getInstance() {
+		return singleton;
+	}
+
+	public synchronized int getNextTicketNumber() {
+		return ticket++;
+	}
+}
